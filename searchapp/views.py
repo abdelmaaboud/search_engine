5a821fcbdddf4663ac9acb2d53c1a5ced696aeb6
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2017-05-10 14:48:22 +0200

    Add files via upload
    Changes in views  
    search in soundex by multi word 
    search in Spelling Correction by multi word

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,63 +1,45 @@
-from django.shortcuts import render,render_to_response
-import json
+from django.shortcuts import render,render_to_response,HttpResponse
+from twisted.conch.insults.insults import privateModes
 from .models import Document,Inverted_Index_Element,SoundexTerm,BiGramTerm
-from django.http import HttpResponse,HttpResponseRedirect
-from.models import WebSiteCrawler,Document
 from django.core.paginator import Paginator
-# Create your views here.
-
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
 import string
-
-
 def search(request):
     try :
         if request.GET:
             page = request.GET.get('page', 1)
-            term=request.GET['q']
+            term=request.GET['q'].lower().strip()
+            if term=="":
+                return render_to_response('search.html')
+            #exact
+            elif '"' in term:
+                t=term.strip('"')
+                docs = get_documents(t, True)
+                return render_to_response("result.html",
+                                          {"docs": docs, "term": term, "soundex": request.GET.get("soundex", None),
+                                           "correction": request.GET.get("correction", None)})
+            #soundex
             if request.GET.get("soundex",None) and request.GET.get("soundex",None)!="None":
-                print("soundex")
-                sound = get_soundex(term)
-                print(sound)
-                all_soundex = SoundexTerm.objects.get(soundex=sound)
-                docs =[]
-                print(all_soundex)
-                print("all : ",all_soundex.terms)
-                word_list = all_soundex.terms.split(",")
+                #ret = get_soundex_documents(term,request)
+                ret = get_soundex_terms(term,request)
 
-                for word in word_list:
-                    ## limit of returned documents
-                    if len(docs) > 150:
-                        break
-                    inverted_list = Inverted_Index_Element.objects.filter(term=word)
-                    for inv in inverted_list:
-                        doc = Document.objects.get(pk=inv.doc_id)
-                        if doc not in docs:
-                            docs.append(doc)
-                for word in word_list:
-                    l = list(Document.objects.filter(title__icontains=word))
-                    for d in l:
-                        docs.insert(0, d)
+                return render_to_response("result.html", ret)
 
+
+
+            #spelling correction
             elif request.GET.get("correction",None) and request.GET.get("correction",None)!="None":
-                print("correction")
                 ret = correct(term,request)
                 return render_to_response("result.html",ret )
-
-
-            elif '"' in term:
-                term=term.strip('"')
-                docs = get_documents(term, True)
-
+            #multi keyword
             else:
                 docs = MultiKeywordSearch(term)
                 if docs ==[]:
                     ret = correct(term,request)
                     return render_to_response("result.html", ret)
-
-            print("term " ,term)
+            #split documents to pages
             pages = Paginator(docs,15)
             docs = pages.page(page)
             return render_to_response("result.html",{"docs":docs,"term":term,"soundex":request.GET.get("soundex",None),"correction":request.GET.get("correction",None)})
@@ -65,90 +47,186 @@ def search(request):
             return render_to_response('search.html')
     except Exception as e:
         return HttpResponse("<h2>error</h2><br>"+str(e))
+def get_soundex_terms(term,request):
+    docs = []
+    word_list = word_tokenize(term)
+    if len(word_list) == 1:
+        sound = get_soundex(term)
+        print(sound)
+        all_soundex = SoundexTerm.objects.get(soundex=sound)
+        docs = []
+        result_terms = all_soundex.terms.split(",")
+        print(result_terms)
+    else:
+        corriction_list = []
+        for word in word_list:
+            sound = get_soundex(word)
+            corriction_list.append(SoundexTerm.objects.get(soundex=sound).terms.split(","))
+            print(corriction_list)
+        first_list = corriction_list[0]
+        for i in range(1, len(corriction_list)):
+            first_list = get_words_combination(first_list, corriction_list[i])
+            print(first_list)
+        result_terms = first_list
+    return {"docs": docs, "result_terms": result_terms, "term": term,
+                "soundex": request.GET.get("soundex", None),
+                "correction": request.GET.get("correction", None)}
+def get_soundex_documents(term,request):
+    sound = get_soundex(term)
+    print(sound)
+    all_soundex = SoundexTerm.objects.get(soundex=sound)
+    docs = []
+    word_list = all_soundex.terms.split(",")
+    print(word_list)
+    for word in word_list:
+        ## limit of returned documents
+        if len(docs) > 200:
+            break
+        inverted_list = Inverted_Index_Element.objects.filter(term=word)
+        for inv in inverted_list:
+            doc = Document.objects.get(pk=inv.doc_id)
+            if doc not in docs:
+                docs.append(doc)
+    for word in word_list:
+        l = list(Document.objects.filter(title__icontains=word))
+        for d in l:
+            docs.insert(0, d)
+
+    return {"docs": docs, "term": term,
+                "soundex": request.GET.get("soundex", None),
+                "correction": request.GET.get("correction", None)}
 
 def correct(term,request):
+    docs = []
+    word_list = word_tokenize(term)
+    if len(word_list) > 1 :
+        corriction_list=[]
+        for word in word_list:
+            corriction_list.append(get_correction_terms(word))
+            print(corriction_list)
+        first_list = corriction_list[0]
+        for i in range(1,len(corriction_list)):
+            first_list= get_words_combination(first_list, corriction_list[i])
+            print(first_list)
+        result_terms = first_list
+        return {"docs": docs, "result_terms": result_terms,"term":term,"soundex":request.GET.get("soundex",None),"correction":request.GET.get("correction",None)}
+
+    else :
+        result_terms = get_correction_terms(term)
+
+        return {"docs": docs, "result_terms": result_terms,"term":term,"soundex":request.GET.get("soundex",None),"correction":request.GET.get("correction",None)}
+def get_words_combination(list1,list2):
+    result = []
+    for i in list1:
+        for j in list2:
+            result.append(i+" "+ j)
+    print(result)
+    return result
+
+def get_correction_terms(term):
     bigrams = get_bigram(term)
-    print(bigrams)
-    print(len(bigrams))
     all_terms = []
     for bi in bigrams:
         print(bi)
         terms = BiGramTerm.objects.get(bigram=bi).terms.split(",")
-        print("terms : ", terms, str(len(terms)))
         for t in terms:
             if t not in all_terms and checkBiGram(term, t):
-                print("t : ", t)
                 all_terms.append(t)
-    print("all : ", all_terms)
-    print(len(all_terms))
     ### will add edite distance
     distance = dict()
     mini_dsitance = 50
-    min_term = ""
     for t in all_terms:
         distance[t] = EditDistance(t, term)
-        print(t, distance[t])
         if mini_dsitance > distance[t]:
             mini_dsitance = distance[t]
-            min_term = t
-    print(min_term)
-    docs = []
+
 
     result_terms = []
     for t in distance:
-        print(t)
-        print(distance[t])
         if distance[t] == mini_dsitance:
             result_terms.append(t)
-    print("result : ", result_terms)
-    return {"docs": docs, "result_terms": result_terms,"term":term,"soundex":request.GET.get("soundex",None),"correction":request.GET.get("correction",None)}
-
-
-
+    return result_terms
 def MultiKeywordSearch(keyword):
-    print("MultiKeywordSearch ",keyword)
     word_list = preproccess(keyword)
-    docs=get_documents(keyword)
-    """
-    for word in word_list:
-        inverted_list=Inverted_Index_Element.objects.filter(term=word)
-        for inv in inverted_list :
-            print(inv.doc_id)
-            docs.append(Document.objects.get(pk=inv.doc_id))
-    """
-    ## call ranking function !!
-    # then add in start all documents have term in title
-
-    for word in word_list:
-        l = list(Document.objects.filter(title__icontains=word))
-        print(l)
-        for d in l :
-            docs.insert(0,d)
+    docs = []
+    l = list(Document.objects.filter(title__icontains=keyword))
+    for d in l:
+        docs.append(d)
+    docs_returned= get_documents(keyword)
+    if len(word_list)==1:
+        return docs + docs_returned
+    print(docs_returned)
+    rank = Rank(keyword,docs_returned)
+    print(rank)
+    for r in rank:
+        doc = Document.objects.get(pk=r)
+        if doc not in docs:
+            docs.append(doc)
     return docs
-
+#get all documents (exact search or multikeyword)
 def get_documents(keyword,exact= False):
     word_list = preproccess(keyword)
     all_inverted_list=[]
     big_list=[]
     for word in word_list:
         inverted_list=Inverted_Index_Element.objects.filter(term=word)
-        all_inverted_list.append(inverted_list)
+        all_inverted_list.append(list(inverted_list))
         docs_id_list=[]
         for i in inverted_list:
             docs_id_list.append(i.doc_id)
         big_list.append(docs_id_list)
     intersect=big_list[0]
+    docs = []
     for i in range(len(big_list)):
         intersect = list(set(big_list[i]) & set(intersect))
-    docs=list(Document.objects.filter(id__in=intersect))
-    print(docs)
-    if not exact :
+    if len (word_list) ==1:
+        rank = dict()
+        for inv in inverted_list:
+            rank[inv.doc_id] =inv.freq
+        b = [(k, rank[k]) for k in sorted(rank, key=rank.get, reverse=True)]
+        print(b)
+        for i in b:
+            doc = Document.objects.get(pk=i[0])
+            docs.append(doc)
+        print(docs)
         return docs
 
 
+    docs=list(Document.objects.filter(id__in=intersect))
 
 
+    if not exact :
+        return docs
+    if intersect==[]:
+        return []
+    rank = dict()
+    inverted_list = []
+    for word in word_list:
+        inverted = list(Inverted_Index_Element.objects.filter(term=word, doc_id__in=intersect))
+        print("len : ",word , len(inverted))
+        inverted_list.append(inverted)
 
+    for j in range (len(inverted_list[0])):
+        positions= []
+        for i in range(len(inverted_list)):
+            pos =list(map(int ,inverted_list[i][j].positions.strip("[").strip("]").split(",")))
+            positions.append(pos)
+            id = inverted_list[i][j].doc_id
+        r= calculateRanking_exact(positions)
+
+        if r >0:
+            rank[id] =r
+            print(id , " rank : ",r )
+
+    docs = []
+    b = [(k, rank[k]) for k in sorted(rank, key=rank.get, reverse=True)]
+    print(b)
+
+    for i in b:
+        doc= Document.objects.get(pk=i[0])
+        docs.append(doc)
+    print(docs)
+    return docs
 def EditDistance(word1,word2):
     m = len(word1)
     n = len(word2)
@@ -164,7 +242,6 @@ def EditDistance(word1,word2):
             else:
                 Matrix[i][j] = 1 + min(Matrix[i][j - 1], Matrix[i - 1][j], Matrix[i - 1][j - 1])
     return Matrix[m][n]
-
 def get_bigram(word):
     word = '$' + word + '$'
     count = 0
@@ -174,8 +251,6 @@ def get_bigram(word):
         list.append(word[count] + word[count+1])
         count += 1
     return list
-
-
 def checkBiGram(word1,word2):
     list_word1 = get_bigram(word1)
     list_word2 = get_bigram(word2)
@@ -184,16 +259,11 @@ def checkBiGram(word1,word2):
     union = list_word1+list_word2
     union_len = len(union)
     JC = intersect_len / (union_len-intersect_len)
-
-
-
     if JC >= 0.45:
         return True
     else:
         return False
-
-
-
+#preprocessing
 def preproccess(text):
     punc = string.punctuation + "^"
     text = text.lower()
@@ -204,12 +274,12 @@ def preproccess(text):
     return_list=[]
 
     for word in word_list:
-        try:
-            return_list.append(stemmer.stem(word))
-        except:
-            continue
+        if word not in stopwords.words('english'):
+            try:
+                return_list.append(stemmer.stem(word))
+            except:
+                continue
     return return_list
-
 def EditDistance(word1,word2):
     m = len(word1)
     n = len(word2)
@@ -225,7 +295,6 @@ def EditDistance(word1,word2):
             else:
                 Matrix[i][j] = 1 + min(Matrix[i][j - 1], Matrix[i - 1][j], Matrix[i - 1][j - 1])
     return Matrix[m][n]
-
 def get_soundex(word):
     new_word = word[0] + ""
     for i in range(1,len(word)):
@@ -263,24 +332,145 @@ def get_soundex(word):
         for i in range(len(word_filtered),4):
             word_returned += "0"
     return word_returned
+def RankingAlgorithm(searchquery, documents):
+    searchquery_split=searchquery.split()
 
+    dic =dict()
+    query_term_dict=dict()
+    l=list()
+    for query_term in searchquery_split:
+        for doc_number in documents.keys():
+            doc=documents[doc_number].split()
+            for i in range(len(doc)):
+                if query_term==doc[i]:
+                    dic={doc_number:i}
+                    l.append(dic)
+        query_term_dict[query_term]=l
+        l=[]
+    dic2=query_term_dict
+    #print query_term_dict
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the search index.")
-def crawl(request):
+    l=[]
 
-    webs = WebSiteCrawler.objects.all()
-    for w in webs:
-        w.crawl()
-def get_terms(request):
-    docs = Document.objects.all()
-    for doc in docs[291:600]:
-        doc.get_terms()
-        print(doc.pk)
-def get_index(request):
-    docs = Document.objects.all()
-    for doc in docs [292:]:
-        inverted = doc.get_index()
-        print(doc.pk)
+    dic2=calculateDistance(query_term_dict, searchquery_split,documents)
 
-    return HttpResponse(str(inverted))
+    l=[]
+    l_tmp1=[]
+    l_tmp2=[]
+    final_rank=[]
+    for d in dic2.keys():
+        l.append(dic2[d])
+
+    l_tmp1.sort()
+
+    l.sort()
+    for i in range(len(l)):
+       for d in dic2.keys():
+          if l[i]==dic2[d]:
+             final_rank.append(d)
+             del dic2[d]
+             break
+
+    return final_rank
+def calculateDistance(dic, searchquery,documetns):
+   distance_dict=dict()
+   distance_list=[]
+   q_term1_list=dic[searchquery[0]]
+   q_term2_list=dic[searchquery[1]]
+   q_term1_listx=[]
+   q_term2_listx=[]
+   for i in range(len(q_term1_list)):
+       dic_tmp=q_term1_list[i]
+       for key in dic_tmp.keys():
+         q_term1_listx.append([key,dic_tmp[key]])
+   for i in range(len(q_term2_list)):
+       dic_tmp = q_term2_list[i]
+       for key in dic_tmp.keys():
+           q_term2_listx.append([key, dic_tmp[key]])
+   i=0
+   j=0
+   tmp_index=0
+   min_dic = dict()
+   for k in documetns.keys():
+       min_dic[k] = 1000
+
+   while True:
+         l1=q_term1_listx[i]
+         l2=q_term2_listx[j]
+         if l1[0]==l2[0]:
+           if [l1[0],abs(l1[1]-l2[1])] not in distance_list and l1[1]-l2[1]<0:
+               diff = abs(l1[1] - l2[1])
+               if diff < min_dic[l1[0]]:
+                   min_dic[l1[0]] = diff
+                   distance_dict[l1[0]]=diff
+         j+=1
+         if j==len(q_term2_listx):
+             i+=1
+             j=0
+         if  i==len(q_term1_listx):
+             break
+
+   return distance_dict
+def MainRanking(searchquery,documents):
+    searchquery_split=searchquery.split()
+    List_of_terms=[]
+    for i in range(len(searchquery_split)-1):
+        combination=searchquery_split[i]+" "+searchquery_split[i+1]
+        List_of_terms.append(combination)
+    l=[]
+    #print List_of_terms,"*"
+    for term in List_of_terms:
+        l.append(RankingAlgorithm(term,documents))
+    rank_dict=dict()
+    for d in documents.keys():
+        rank_dict[d]=0
+    final_rank=[]
+    max=0
+    save_index=0
+    k=0
+    print(l)
+    while True:
+       for i in range(len(l)):
+            if k==len(l[i]):
+                for d in documents.keys():
+                    if d not in final_rank:
+                        final_rank.append(d)
+                return final_rank
+            rank_dict[l[i][k]]+=1
+            if(rank_dict[l[i][k]]>max):
+                max=rank_dict[l[i][k]]
+                save_index=l[i][k]
+       if save_index not in final_rank:
+          final_rank.append(save_index)
+       for d in documents.keys():
+           rank_dict[d] = 0
+       max=0
+       save_index=0
+       k+=1
+def Rank(query,documents):
+    if documents==[]:
+        return []
+    documents_dict=dict()
+    for doc in documents:
+        documents_dict[doc.id]=doc.text.lower()
+    return MainRanking(query.lower(), documents_dict)
+def calculateRanking_exact(BigPositionsList):
+    count = len(BigPositionsList)
+    if count == 1:
+        return len(BigPositionsList[0])
+    rank = 0
+    for i in BigPositionsList[0]:
+        expected = i+1
+        countR = 1
+        flag = False
+        while countR < count:
+            if expected in BigPositionsList[countR]:
+                expected += 1
+                countR += 1
+                flag = True
+            else:
+                flag = False
+                break
+        if flag == True:
+            rank += 1
+    return rank
